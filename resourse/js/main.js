87c556528e3d72ad50cb2a46cb77e9b3c2ce938f
SD-Palash <skpalash993@gmail.com>
2020-04-14 13:48:44 +0600

    first commit

---
+++ resourse/js/main.js
@@ -0,0 +1,35 @@
+// FOR SMOOTH SCROLL EFFECT 
+$(function () {
+    var topoffset = 55; //variable for menu height
+
+    //Use smooth scrolling when clicking on navigation
+    $('.navbar-nav a').click(function () {
+        if (location.pathname.replace(/^\//, '') ===
+            this.pathname.replace(/^\//, '') &&
+            location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - topoffset
+                }, 500);
+                return false;
+            } //target.length
+        } //click function
+    }); //smooth scrolling
+
+});
+
+
+
+// FOR ACTIVE MIXITUP CLASS 
+var mixer = mixitup('.imageMix');
+
+
+// FOR ACTIVE TIMER CLASS 
+$('.timer').countTo();
+
+
+// FOR WOW JS FILE ACTIVE CODE 
+new WOW().init();
+
